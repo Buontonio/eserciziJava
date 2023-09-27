19bac77f77309f887ebb20225765dfeb4a4ab7bc
AB <antoniobuonanno0605@gmail.com>
2023-09-27 12:09:36 +0200

    5th Commit
    The exercise five has been committed.

---
+++ Ex5/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sum(6, 10));
+    }
+    public static int sum(int x, int y) {
+        int z = x + y;
+        return z;
+    }
+}
