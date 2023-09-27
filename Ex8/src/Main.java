7dad9187b19b90ee280b9cbfe7862063ad28b1cf
AB <antoniobuonanno0605@gmail.com>
2023-09-27 14:42:51 +0200

    8th Commit
    The 8th exercise has been committed.

---
+++ Ex8/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double x=8.5;
+        double y=9.3;
+        double z=7.46;
+        double resultAverage = (x+y+z)/3;
+
+        System.out.println("The average result of the numbers is: " + resultAverage);
+    }
+}
