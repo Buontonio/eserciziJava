a52b084294427c86ec4fe1de070b1e05ec92e3b6
AB <antoniobuonanno0605@gmail.com>
2023-09-27 14:33:59 +0200

    7th Commit
    The 7th exercise has been uploaded.

---
+++ Ex7/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 3;
+
+        int resultDiv = x/y;
+
+        System.out.println("The result of division is: " + resultDiv);
+    }
+}
