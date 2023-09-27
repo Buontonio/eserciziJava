a0026c926120d82ebd0158d05b1a7b3d27b65339
AB <antoniobuonanno0605@gmail.com>
2023-09-27 10:59:52 +0200

    3rd Commit
    The 3rd exercise has been committed.

---
+++ Ex3/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    double x= 4.7;
+    double y= 5.9;
+    int z = (int) x + (int) y;
+
+
+        System.out.println("The sum of the two double variables is: " + z);
+    }
+}
