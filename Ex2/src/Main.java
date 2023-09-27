959f6df863a69b39ac13750637ffbe91db7eea29
AB <antoniobuonanno0605@gmail.com>
2023-09-27 10:54:26 +0200

    2nd Commit
    The 2nd exercise has been committed.

---
+++ Ex2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+char x='a';
+char y='b';
+
+
+        System.out.println("The sum of the two char variables is: " + x+y);
+    }
+}
