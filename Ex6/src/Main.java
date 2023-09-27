ba5780bed0074c56b39cc466f52f163654e6a640
AB <antoniobuonanno0605@gmail.com>
2023-09-27 14:07:22 +0200

    6th Commit
    6th exercise upload

---
+++ Ex6/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "abcde";
+        System.out.println(lunghezza(nome));
+    }
+
+    public static int lunghezza(String x) {
+        return x.length();
+    }
+}
