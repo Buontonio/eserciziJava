e0b04c2c7836f012028a8a73b663043ed2421bb4
AB <antoniobuonanno0605@gmail.com>
2023-09-27 15:43:44 +0200

    10th Commit
    The 10th exercise has been uploaded.

---
+++ Ex9/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=9;
+        int y=9;
+
+        int xInc = x+1;
+        System.out.println("The incremented value of x is: " + xInc);
+        int yInc = y+1;
+        System.out.println("The incremented value of y is: " + yInc);
+
+        int resultingMult = xInc * yInc;
+
+        System.out.println("The result of the multiplication of the incremented values is: " + resultingMult);
+    }
+}
