dffe8407c775360ba79851d988bc548f6b1fe16a
AB <antoniobuonanno0605@gmail.com>
2023-10-09 12:18:52 +0200

    28th Ex update
    This commit updates the 28th exercise.

--- Ex28/src/Main.java
+++ Ex28/src/Main.java
@@ -1,11 +1,21 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[][] matrix = {{1, 2, 3}, {4, 5, 6}};
-        System.out.println(Arrays.deepToString(matrix));
+        int[][] matrix = new int[3][4];
+        System.out.println(Arrays.deepToString(arrayFiller(matrix)));
         System.out.println(Arrays.deepToString(transpose(matrix)));
+    }
+
+    public static int[][] arrayFiller(int[][] array) {
+        for (int i = 0; i< array.length; i++) {
+            for (int j=0; j<array[i].length; j++) {
+                array[i][j] = (int)(Math.random()*10);
+            }
+        }
+        return array;
     }
 
 
