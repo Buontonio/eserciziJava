675e8cefca7fe13d4b339ccabaebd6b5653c11ab
AB <antoniobuonanno0605@gmail.com>
2023-10-10 10:53:18 +0200

    Ex31 update
    This commit updates the 31th exercise.

--- Ex31/src/Main.java
+++ Ex31/src/Main.java
@@ -4,9 +4,5 @@ public class Main {
         Auto car1 = new Auto(3.0, "DD483KT", "Audi", "RS6");
         System.out.println(car1);
 
-        car1.plate = "EF566PT";
-        car1.capacity = "3.5";
-        car1.brand = "Mercedes";
-        car1.model = "GLE";
     }
 }
