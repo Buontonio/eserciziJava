dffa9a5e4b121f74322758bfe51fc699b063b6cd
AB <antoniobuonanno0605@gmail.com>
2023-10-11 17:05:15 +0200

    34th Ex
    This commit uploads the 34th exercise.

---
+++ Ex34/src/Forma.java
@@ -0,0 +1,6 @@
+public class Forma {
+
+    public double calcoloArea(double x, double y) {
+        return x*y/5;
+    }
+}
---
+++ Ex34/src/Main.java
@@ -0,0 +1,15 @@
+//Scrivere un programma che contenga una classe chiamata Forma ed un metodo chiamato calcolaArea() che stampi l'area della forma.
+//        Crea poi una sottoclasse chiamata Rettangolo che fa override del metodo calcolaArea() per calcolare l'area del rettangolo.
+public class Main {
+    public static void main(String[] args) {
+
+        Forma forma = new Forma();
+        System.out.println(forma.calcoloArea(4.2,5.3));
+
+        Rettangolo rettangolo = new Rettangolo();
+        System.out.println(rettangolo.calcoloArea(12.5,2));
+
+        Triangolo triangolo = new Triangolo();
+        System.out.println(triangolo.calcoloArea(5,2));
+    }
+}
