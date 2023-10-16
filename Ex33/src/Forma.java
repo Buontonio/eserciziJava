371ce13d90a541c11fa3ea55bb34d9dd5562a5a7
AB <antoniobuonanno0605@gmail.com>
2023-10-16 17:04:15 +0200

    34th Ex Update
    This commit fixes the 34th exercise.

--- Ex33/src/Forma.java
+++ Ex33/src/Forma.java
@@ -1,6 +1,6 @@
 public class Forma {
 
     public double calcoloArea(double x, double y) {
-        return x*y/2;
+        return 0.0;
     }
 }
--- Ex33/src/Rettangolo.java
+++ Ex33/src/Rettangolo.java
@@ -2,6 +2,6 @@ public class Rettangolo extends Forma{
 
     @Override
     public double calcoloArea(double x, double y) {
-        return x*y/5;
+        return x*y;
     }
 }
