a64ff021bca024e9069e2be56bdc4de554630460
AB <antoniobuonanno0605@gmail.com>
2023-10-20 14:42:06 +0200

    Ex43 update.

--- Ex43/src/Main.java
+++ Ex43/src/Main.java
@@ -8,30 +8,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            Double numeratore = 5.0;
+            Double denominatore = null;
 
-//        System.out.println("Insert first double value: ");
-        Double numeratore = 7.0;
-        Double denominatore = null;
-        System.out.println("Your final result is: ");
-        System.out.println(division(numeratore,denominatore));
+            System.out.println("Your final result is: ");
+            System.out.println(division(numeratore, denominatore));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 
     public static Scanner sc = new Scanner(System.in);
 
     public static double division(Double numeratore, Double denominatore) {
-        while (true) {
-            try {
-//                Double numeratore = sc.nextDouble();
-//                Double denominatore = sc.nextDouble();
-                double result = numeratore / denominatore;
-                return result;
-            } catch (NullPointerException e) {
-                System.out.println("You can't have a \"null\" inside your function.\nPlease verify your operators");
-                sc.next();
-            }catch (InputMismatchException e) {
-                System.out.println("Do not enter characters, integer values needed.\nPlease restart: ");
-                sc.next();
-            }
+
+        if ((numeratore == null) || (denominatore == null)) {
+            throw new NullPointerException("You can't have a \"null\" inside your function.\nPlease verify your operators");
         }
+        if (denominatore == 0) {
+            throw new ArithmeticException("The denominator can't be eqaul to 0");
+        }
+        return numeratore/denominatore;
     }
 }
