7d355112e8f2cd7b91afd57d6108ff1d4a1b444e
AB <antoniobuonanno0605@gmail.com>
2024-01-20 12:37:49 +0100

    Ex1 correction update

--- Spring/2. Nostra prima applicazione/Ex1/src/main/java/com/example/Ex1/NameController.java
+++ Spring/2. Nostra prima applicazione/Ex1/src/main/java/com/example/Ex1/NameController.java
@@ -1,11 +1,7 @@
 package com.example.Ex1;
 
 import com.example.Ex1.DTO.NameDTO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.support.SimpleTriggerContext;
 import org.springframework.web.bind.annotation.*;
-
-import javax.naming.Name;
 
 @RestController
 @RequestMapping(path = "/v1")
@@ -13,16 +9,15 @@ import javax.naming.Name;
 public class NameController {
 
 
-    @GetMapping(path = "/name{name}")
+    @GetMapping(path = "/name/{name}")
     public String returnName(@PathVariable String name) {
         return name;
     }
 
-    @PostMapping(path = "/reversedName")
-    public StringBuilder reversedName(@RequestBody NameDTO nameDTO) {
 
-
-        StringBuilder finalWord = new StringBuilder(((nameDTO.getName())));
+    @PostMapping(path = "/reversedName/{name}")
+    public StringBuilder reversedName(@PathVariable String name) {
+        StringBuilder finalWord = new StringBuilder(((name)));
         return finalWord.reverse();
     }
 }
