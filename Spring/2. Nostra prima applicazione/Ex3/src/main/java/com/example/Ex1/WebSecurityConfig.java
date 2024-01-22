17a78d3b7253512c66b4381746ad934984bce619
AB <antoniobuonanno0605@gmail.com>
2024-01-22 10:47:50 +0100

    3rd Ex update

---
+++ Spring/2. Nostra prima applicazione/Ex3/src/main/java/com/example/Ex1/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.Ex1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+public class WebSecurityConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080");
+            }
+        };
+    }
+}
